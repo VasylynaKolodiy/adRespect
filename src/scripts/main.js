15ecb6606bd3d92d90a7067823e2a35c69e01129
velisssa <tybinka.vasylyna@gmail.com>
2023-08-20 21:40:03 +0200

    add other sections, work with navbar

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,3 +2,23 @@ AOS.init({
   duration: 1200,
   once: true
 });
+let msnry;
+
+document.addEventListener("DOMContentLoaded", () => {
+  msnry = new Masonry('.projects__items', {
+    percentPosition: true
+  });
+});
+
+const showMoreBtn = document.querySelector('.projects .showMore');
+const projects = document.querySelectorAll('.projects__items .projects__item');
+const overlay = document.querySelector('.projects .overlay');
+
+showMoreBtn.addEventListener('click', () => {
+  for (let i = 9; i < projects.length; i++){
+    projects[i].classList.remove('hide');
+    showMoreBtn.classList.add('hide');
+    overlay.classList.add('hide');
+  }
+  msnry.layout();
+})
